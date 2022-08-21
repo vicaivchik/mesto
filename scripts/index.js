43a61d199190541760f598a42d511dd2ac7602aa
Viktoryia Iuchyk <victoriaivchik@gmail.com>
2022-08-21 15:04:32 +0300

    all

---
+++ scripts/index.js
@@ -0,0 +1,69 @@
+const likes = document.querySelectorAll('.card__like');
+
+for (let i = 0; i < likes.length; i++) {
+    likes[i].addEventListener('click', toggleLike);
+}
+
+function toggleLike(event) {
+    event.target.classList.toggle('card__like_active');
+}
+
+const buttonEdit = document.querySelector('.profile__button-edit');
+
+buttonEdit.addEventListener('click', editProfile);
+
+function editProfile() {
+    const name = document.querySelector('.profile__name').textContent;
+    const work = document.querySelector('.profile__work').textContent;
+
+    const nameInput = document.querySelector('.pop-up__input_type_name');
+    const workInput = document.querySelector('.pop-up__input_type_work');
+
+    nameInput.value = name;
+    workInput.value = work;
+
+    const popUp = document.querySelector('.pop-up');
+    popUp.classList.add('pop-up_opened');
+}
+
+const popUpClose = document.querySelector('.pop-up__close');
+
+popUpClose.addEventListener('click', closePopup);
+
+function closePopup() {
+    const popUp = document.querySelector('.pop-up');
+    popUp.classList.remove('pop-up_opened');
+}
+
+
+// Находим форму в DOM
+const formElement = document.querySelector('.pop-up__form');
+// Находим поля формы в DOM
+const nameInput = document.querySelector('.pop-up__input_type_name');
+const jobInput = document.querySelector('.pop-up__input_type_work');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    const jobVal = jobInput.value;
+    const nameVal = nameInput.value;
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const name = document.querySelector('.profile__name');
+    const work = document.querySelector('.profile__work');
+
+    // Вставьте новые значения с помощью textContent
+    name.textContent = nameVal;
+    work.textContent = jobVal;
+
+    closePopup();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
